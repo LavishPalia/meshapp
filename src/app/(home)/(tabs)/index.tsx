3e1456b856403776911aa2616a2eff817aa7ded7
Lavish Palia <lavishgarg1199@gmail.com>
2024-09-08 16:59:29 +0530

    Setup Stream Chat frontned components

--- src/app/(home)/(tabs)/index.tsx
+++ src/app/(home)/(tabs)/index.tsx
@@ -1,11 +1,14 @@
-import { View, Text } from 'react-native';
 import React from 'react';
+import { ChannelList } from 'stream-chat-expo';
+import { router } from 'expo-router';
 
 const MainTabScreen = () => {
   return (
-    <View>
-      <Text>Chat</Text>
-    </View>
+    <ChannelList
+      onSelect={(channel) => {
+        router.push(`/channel/${channel.cid}`);
+      }}
+    />
   );
 };
 
---
+++ src/app/(home)/channel/[cid].tsx
@@ -0,0 +1,44 @@
+import { useLocalSearchParams } from 'expo-router';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Channel as ChannelType } from 'stream-chat';
+import {
+  Channel,
+  MessageInput,
+  MessageList,
+  useChatContext,
+} from 'stream-chat-expo';
+
+const ChannelScreen = () => {
+  const [channel, setChannel] = useState<ChannelType | null>(null);
+
+  const { cid } = useLocalSearchParams<{ cid: string }>();
+
+  const { client } = useChatContext();
+
+  useEffect(() => {
+    const fetchChannel = async () => {
+      const channels = await client.queryChannels({ cid });
+      setChannel(channels[0]);
+    };
+
+    fetchChannel();
+  }, [cid]);
+
+  if (!channel) {
+    return <ActivityIndicator />;
+  }
+
+  return (
+    <Channel channel={channel}>
+      <MessageList />
+      <SafeAreaView edges={['bottom']}>
+        <MessageInput />
+      </SafeAreaView>
+      {/* <MessageInput /> */}
+    </Channel>
+  );
+};
+
+export default ChannelScreen;
